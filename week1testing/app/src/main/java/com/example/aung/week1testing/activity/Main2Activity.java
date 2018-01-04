f13eaa3599214164241c43011dee3ce1a2c777ed
Zin Min Phyoe <zinminphyo075@gmail.com>
2018-01-04 18:13:48 +0630

    update

---
+++ week1testing/app/src/main/java/com/example/aung/week1testing/activity/Main2Activity.java
@@ -0,0 +1,15 @@
+package com.example.aung.week1testing.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.aung.week1testing.R;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
